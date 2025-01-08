0f52eb6af147ce1ae56060ec40d2aedf9db3dd5c
arturbernardo <artur.bernardo.k@gmail.com>
2025-01-08 19:41:27 -0300

    animation walking setup end

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -5,6 +5,9 @@ const friction = 400
 var input = Vector2.ZERO
 
 @export var speed = 80
+@onready var anim_tree: AnimationTree = $AnimationTree
+@onready var anim_state = anim_tree.get("parameters/playback")
+
 
 func _process(delta: float) -> void:
 	player_movement(delta)
@@ -18,13 +21,21 @@ func diagonal_movement(diagonal):
 func player_movement(delta): 
 	input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if input != Vector2.ZERO:
+		animation_state(input)
+		anim_state.travel("Move")
 		velocity = velocity.limit_length(speed)
 	
 	if input == Vector2.ZERO:
 		if velocity.length() > (friction * delta):
 			velocity -= velocity.normalized() * (friction * delta)
 		else:
+			anim_state.travel("Idle")
 			velocity = Vector2.ZERO
 		
 	velocity += diagonal_movement(input * acceleration * delta)
 	move_and_slide()
+	
+func animation_state(input):
+	anim_tree.set("parameters/Idle/blend_position", input)
+	anim_tree.set("parameters/Move/blend_position", input)
+	anim_tree.set("parameters/Attack/blend_position", input)
