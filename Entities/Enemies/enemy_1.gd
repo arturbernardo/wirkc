e20eb7c504fa47189ae9f13e3ae9c6e92601a152
arturbernardo <artur.bernardo.k@gmail.com>
2025-01-11 18:56:36 -0300

    enemy random move

---
+++ Entities/Enemies/enemy_1.gd
@@ -0,0 +1,54 @@
+extends CharacterBody2D
+var current_direction = Vector2()
+var change_direction
+@export var speed = 20
+
+func _ready():
+	change_direction = 3
+
+func _process(delta: float) -> void:
+	petrolling(delta)
+
+func petrolling(delta):
+	if (change_direction == 0):
+		move_up(delta)
+	if (change_direction == 1):
+		move_down(delta)
+	if (change_direction == 2):
+		move_left(delta)
+	if (change_direction == 3):
+		move_right(delta)
+		
+func diagonal_movement(diagonal):
+	var screen_pos = Vector2()
+	screen_pos.x = diagonal.x - diagonal.y
+	screen_pos.y = (diagonal.x + diagonal.y)
+	return screen_pos
+
+func move_up(delta):
+	velocity += diagonal_movement(Vector2.UP * speed * delta)
+	$anim.play("move_up_right")
+	move_and_slide()
+	
+func move_down(delta):
+	velocity += diagonal_movement(Vector2.DOWN * speed * delta)
+	$anim.play("move_down_left")
+	move_and_slide()
+
+func move_left(delta):
+	$anim.play("move_down_left")
+	velocity += diagonal_movement(Vector2.LEFT * speed * delta)
+	move_and_slide()
+	
+func move_right(delta):
+	$anim.play("move_down_right")
+	velocity += diagonal_movement(Vector2.RIGHT * speed * delta)
+	move_and_slide()
+
+func _on_timer_timeout() -> void:
+	velocity = lerp(velocity, Vector2.ZERO, 1)
+	change_path()
+	$Timer.start()
+
+func change_path():
+	change_direction = randi() % 4
